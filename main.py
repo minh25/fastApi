0a7f57f90b541a16aba4b290b6da280b77fe2755
minh25 <minh00012258@gmail.com>
2024-06-09 19:02:05 +0700

    config log

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    logger.info(f"request / endpoint!")
     return {"message": "Hello World"}
 
 
